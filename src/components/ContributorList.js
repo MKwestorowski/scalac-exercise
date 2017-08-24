77cc5e3d84b40f200f5ab39ecbb42d24785284f9
Michał <kwestorowski.m@gmail.com>
2017-08-24 23:11:08 +0200

    Sorting at contributions count works

--- src/components/ContributorList.js
+++ src/components/ContributorList.js
@@ -23,19 +23,72 @@ export default connect(
 )(class Singin extends React.Component {
 
 
+    state = {
+        sortingOrder: null,
+
+    }
+
+
+    handleSortingToggle = () => this.setState({
+        sortingOrder: [null, 'ASC'].includes(this.state.sortingOrder) ? 'DESC' : 'ASC'
+    })
+
+
     render() {
         const contributors = this.props.contributors.data
         const contributorProfile = this.props.contributorProfile
+
+const contributorsVariants =  {
+            'DESC': () => contributors.slice().sort(
+                (a, b) => b.contributions - a.contributions
+            ),
+            'ASC': () => contributors.slice().sort(
+                (a, b) => a.contributions - b.contributions
+            )
+        }
+
+        const preparedContributors = (
+            contributorsVariants[this.state.sortingOrder] ||
+            (
+                () => contributors
+            )
+        )()
         return (
-            <div>
-                { contributors === null ?
+
+<div>
+            <table>
+            <thead>
+            <tr>
+                <th>Login</th>
+                <th>
+                    Avatar
+                </th>
+                <th onClick={this.handleSortingToggle}>
+                    Count contributors
+                </th>
+            </tr>
+            </thead>
+            <tbody>
+            {
+                preparedContributors === null ?
                     'Fetching' :
-                    contributors.map(
-                        eachContributor => <div><Link to={'/contributeprofile'}
-                                                      onClick={() => contributorProfile(eachContributor)}
-                                                      key={eachContributor.id}>{eachContributor.login}</Link> <img
-                            src={eachContributor.avatar_url} alt=""/></div>)}
-            </div>
+                    preparedContributors.map(
+                        eachContributor => (
+                            <tr key={eachContributor.id}>
+                                <td><Link to={'/contributeprofile'}
+                                          onClick={() => contributorProfile(eachContributor)}
+                                          key={eachContributor.id}>{eachContributor.login}</Link></td>
+                                <td><img
+                                    src={eachContributor.avatar_url} alt=""/></td>
+                                <td>{eachContributor.contributions}</td>
+                            </tr>
+                        )
+                    )
+            }
+            </tbody>
+        </table>
+</div>
+
 
 
 
